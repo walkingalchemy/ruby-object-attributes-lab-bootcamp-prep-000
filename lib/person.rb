c4349fdf1f1a981674c8a4da9fc49e8e05ea6303
James Royer <walkingalchemy@gmail.com>
2017-10-14 03:13:02 +0000

    automatically backed up by learn

---
+++ lib/person.rb
@@ -0,0 +1,15 @@
+class Dog
+  def name=(dog_name)
+    @name = dog_name
+  end
+  def name
+    puts @name
+  end
+
+  def breed=(breed)
+    @breed = breed
+  end
+  def breed
+    puts @breed
+  end
+end
